81f3dd08279f189a0e0f2e237edc4410e15cc494
thejayadad <74875595+thejayadad@users.noreply.github.com>
2023-12-30 09:09:21 -0500

    update

--- myapp/components/client/Navbar/Menu.js
+++ myapp/components/client/Navbar/Menu.js
@@ -1,42 +1,66 @@
 'use client'
 import Link from 'next/link';
-import React, { useState } from 'react';
+import { usePathname } from 'next/navigation';
+import React from 'react';
 import { FiMenu, FiX } from 'react-icons/fi';
 
 const Menu = () => {
-    const [isDrawerOpen, setDrawerOpen] = useState(false);
+  const pathName = usePathname();
+  const links = [
+    {
+      title: 'Homepage',
+      path: '/',
+    },
+    {
+      title: 'About',
+      path: '/about',
+    },
+    {
+      title: 'Contact',
+      path: '/contact',
+    },
+    {
+      title: 'Blog',
+      path: '/blog',
+    },
+  ];
 
-    const handleToggleDrawer = () => {
-      setDrawerOpen(!isDrawerOpen);
-    };
   return (
-            <div className="drawer">
-        <input id="my-drawer" type="checkbox" className="drawer-toggle" />
-        <div className="drawer-content">
-            <label htmlFor="my-drawer" className=" drawer-button"><FiMenu size={30} /></label>
-        </div> 
-        <div className="drawer-side">
-            <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
-    
-            <div className="menu p-4 gap-2 w-80  flex flex-col min-h-full  bg-base-200 text-base-content">
-            <div className='bg-orange-200  p-2 h-48'>
-                Logo
-                ldfnibakl
-                aslkna
-                asnklva
-                </div> 
-                <div className='bg-orange-200 flex flex-col gap-8 p-2 h-48'>
-                <Link href={'/'}>Home</Link>
-                <Link href={'/'}>Home</Link>
-                <Link href={'/'}>Home</Link>
-                </div>
-                <div className='bg-orange-200 p-2 h-64'>
-                Footer
-                </div>       
-            </div>
-        </div>
-        </div>
-  )
-}
+    <div className="drawer">
+      <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content">
+        <label htmlFor="my-drawer" className="drawer-button">
+          <FiMenu size={30} />
+        </label>
+      </div>
+      <div className="drawer-side">
+        <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
 
-export default Menu
+        <div className="menu p-4 gap-2 w-80 flex flex-col min-h-full bg-base-200 text-base-content">
+          {/* Logo Section */}
+          <div className='bg-orange-200 p-2 h-20 flex items-center justify-center'>
+            <h1 className="text-2xl font-bold text-white">FanHub</h1>
+          </div>
+
+          {/* NavLinks Section */}
+          <div className='bg-orange-200 flex flex-col gap-4 p-2 flex-1'>
+            {links.map((link) => (
+              <Link key={link.path} 
+              className={`link ${pathName === link.path ? 'active-link' : ''}`}
+              href={link.path}>
+                {link.title}
+              </Link>
+            ))}
+          </div>
+
+          {/* Footer Section */}
+          <div className='p-2 h-16 flex items-center justify-center'>
+            <p className="text-sm text-gray-800">© 2023 FanHub. All rights reserved.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Menu;
--- myapp/components/client/Navbar/index.js
+++ myapp/components/client/Navbar/index.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import Menu from './Menu'
-import Logo from '@/components/Logo'
+import AuthLinks from './AuthLinks'
 
 const Navbar = () => {
   return (
     <header className='px-4 py-8'>
         <nav className='flex justify-between mx-auto max-w-screen-xl'>
             <Menu />
-            <Logo />
+            <AuthLinks />
         </nav>
     </header>
   )
